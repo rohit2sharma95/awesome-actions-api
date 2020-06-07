7c0a073a1167448bb63954b1aead93bb46c5f809
Rohit Sharma <rohit2sharma95@gmail.com>
2020-06-07 14:50:57 +0530

    :art: Read table of content

---
+++ app.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const { README_URL, REPO_URL } = require('./lib/constants');
+
+(async () => {
+  const { data: readmeHTML } = await axios.get(README_URL);
+  const $ = cheerio.load(readmeHTML);
+  const $contentsList = $('#user-content-contents').parent().next('ul');
+  const getContent = ($mainList) => $mainList.children('li').map((index, elem) => {
+    const $anchor = $(elem).children('a');
+    const $childUl = $(elem).children('ul');
+    return {
+      title: $anchor.text(),
+      url: REPO_URL + $anchor.attr('href'),
+      children: $childUl.length ? getContent($childUl) : null
+    };
+  }).get();
+  const data = getContent($contentsList);
+  if (fs.existsSync('./data')) {
+    fs.rmdirSync('./data', { recursive: true });
+  }
+  fs.mkdirSync('./data');
+  fs.writeFile('./data/content.json', JSON.stringify(data, null, '\t'), null, () => {
+    console.log('Data updated successfully');
+  });
+})();
